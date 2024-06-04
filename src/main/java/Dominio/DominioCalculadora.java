2b7a5411292b6f909b01c3da9e804cb1e2f3a3a1
FquesadaC <fabian03qc@gmail.com>
2024-06-04 17:34:55 -0600

    Clase dominioClaculadora, validarParentesis
    se agrego metodo para validar los parentesis

---
+++ src/main/java/Dominio/DominioCalculadora.java
@@ -0,0 +1,45 @@
+package Dominio;
+
+import java.util.Stack;
+
+public class DominioCalculadora {
+
+    public static boolean validarParentesis(String expresion) {
+        boolean correcta = true;
+
+        Stack<Character> datos = new Stack<>();
+
+        int i = 0;
+        while (correcta && i < expresion.length()) {
+            char actual = expresion.charAt(i);
+            if (actual == '(') {
+                datos.push(actual);
+            } else if (actual == ')') {
+                if (!datos.isEmpty()) {
+                    char top = datos.peek();
+                    if (top == '(') {
+                        datos.pop(); // Eliminar el paréntesis correspondiente
+                    } else {
+                        correcta = false;
+                    }
+                } else {
+                    correcta = false;
+                }
+            }
+            // Se ignoran otros caracteres
+            i++;
+        }
+
+        if (!datos.isEmpty()) {
+            correcta = false;
+        }
+
+        return correcta;
+    }
+
+}
+
+
+
+
+
