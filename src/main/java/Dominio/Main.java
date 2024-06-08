fe00c347c6360cddd45580701f3f2207c132fc8b
FquesadaC <fabian03qc@gmail.com>
2024-06-07 18:13:08 -0600

    Version final para entrega ll

--- src/main/java/Dominio/Main.java
+++ src/main/java/Dominio/Main.java
@@ -8,6 +8,10 @@ public class Main {
     public static void main(String[] args) {
 
         EvaluadorPosfijo evaluadorPosfijo = new EvaluadorPosfijo();
+        ValidarExpresion validarExpresion = new ValidarExpresion();
+
+        System.out.println(validarExpresion.validarParentesis("(8000+2)/46*3"));
+        //System.out.println(validarExpresion.validarParentesis("(8000+2)/46*3)")); //false
 
         System.out.println(Arrays.toString(evaluadorPosfijo.convertirAExpresionPosfija("(8000+2)/46*3")));
 
